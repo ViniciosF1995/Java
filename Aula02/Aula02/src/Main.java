2cfd0aa215356f60b640628bfacfd1083d13f833
VInicios <ferreiradossantosvinicios@gmail.com>
2024-04-11 20:56:05 -0300

    Exercicio 01 e 02

--- Aula02/Aula02/src/Main.java
+++ Aula02/Aula02/src/Main.java
@@ -1,15 +1,52 @@
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//        String s1, s2;
+//
+//        s1= JOptionPane.showInputDialog("S1: ");
+//        s2= JOptionPane.showInputDialog("S2: ");
+//
+//        if(s1.equals(s2)){
+//            System.out.println("Iguais");
+//
+//        }else {
+//            System.out.println("Diferentes");
+//        }
+
+//Exercicio 01
+//        int num, valor = -1;
+//
+//        for(int i=0; i<10; i++){
+//
+//            num = Integer.parseInt(JOptionPane.showInputDialog((i+1) + "Numero: ")) ;
+//
+//            if(num> valor) {
+//                valor = num;
+//            }
+//            System.out.println("O valor maior é: " + valor);
+//        }
+
+//=================================================================
+
+        //Exercicio 02
+
+        int num, valor = -1;
+
+        for(int i=0; i<10; i++) {
+            do {
+                num = Integer.parseInt(JOptionPane.showInputDialog((i + 1) + "Numero: "));
+            }while (num<0 || num>100) ;
+                if (num > valor) {
+                    valor = num;
+                }
+            }
+                System.out.println("O valor maior é: " + valor);
+
         }
     }
-}
+
+
