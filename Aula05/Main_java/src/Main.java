375320a0dc93d6efbcf8f69ca2ba16f76a00f11d
VInicios <ferreiradossantosvinicios@gmail.com>
2024-05-23 21:42:39 -0300

    Aula 5 de feitos em sala de aula herança

---
+++ Aula05/Main_java/src/Main.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Pessoa> pessoas = new ArrayList<>();
+        Aluno aluno1 = new Aluno("Jozefina", 1, "1234", "História");
+        //System.out.println(aluno1.getCurso());
+        //System.out.println(aluno1.getNome());
+        pessoas.add(aluno1);
+       // System.out.println(pessoas.getFirst().getNome());
+        //System.out.println(pessoas.getFirst().getId());
+        //System.out.println(pessoas.getFirst().getSenha());
+
+
+    //Aluno aluno2 = (Aluno) pessoas.getFirst();
+       // System.out.println(aluno2.getCurso());
+
+        Professor prof1 = new Professor("Gleimon", 2, "Rb9*4--Lol","BD");
+        pessoas.add(prof1);
+
+        Aluno aluno3 = new Aluno("Flash", pessoas.size()+1, "onibus", "React de videos de k pop");
+        pessoas.add(aluno3);
+
+        Professor prof2 = new Professor("Tia Vera", 4, "3,14", "Matematica");
+        pessoas.add(prof2);
+
+        for(Pessoa p: pessoas){
+            System.out.println("___________");
+            System.out.println(pessoas.indexOf(p));
+            System.out.println(p.getNome());
+            System.out.println(p.getId());
+            System.out.println(p.getSenha());
+            if( p instanceof Aluno){
+//                System.out.println(p.getNome() + " é um aluno");
+                Aluno a1 = (Aluno) p;
+                System.out.println(a1.getCurso());
+            }else if(p instanceof Professor){
+                Professor p1 = (Professor) p;
+                System.out.println(p1.getArea());
+
+                }
+            }
+
+        }
+
+    }
---
+++ Aula05/Main_java/src/Professor.java
@@ -0,0 +1,20 @@
+public class Professor extends Pessoa{
+
+    private String area;
+
+    public Professor(String nome, int id, String senha, String area) {
+        super(nome, id, senha);
+        this.area = area;
+    }
+
+    public void fazerChamada(){
+        System.out.println("Fazer chamada da turma de " + this.getArea());
+    }
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+}
